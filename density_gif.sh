5f1a41213f0725742ad55169af16a641a3bed1cd
mac <mac@macdeMac.local>
2020-06-10 14:59:29 +0800

    a

---
+++ density_gif.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#SBATCH -o slurm-%j-%N.out
+#SBATCH -A hpc0006177081
+#SBATCH --partition=C032M0256G
+#SBATCH -J txtbak
+#SBATCH --get-user-env
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --qos=low
+
+module load anaconda/3.7.1
+python  density_gif.py
---
+++ test.sh
@@ -0,0 +1,2 @@
+module load anaconda/3.7.1
+python E_wigner_n_r.py
